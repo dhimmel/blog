3b991be05735aae92fa2b2b03570e6a92185032a
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2024-06-29 21:27:45 -0400

    pre-commit checks and ruffen up

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,30 +1,30 @@
 # Python 3 pelican configuration file for blog.dhimmel.com
 
-AUTHOR = 'Daniel Himmelstein'
+AUTHOR = "Daniel Himmelstein"
 SITENAME = "Satoshi Village"
 
 DELETE_OUTPUT_DIRECTORY = True
 
-PATH = 'content'
-ARTICLE_PATHS = ['posts']
-STATIC_PATHS = ['favicon.ico']
+PATH = "content"
+ARTICLE_PATHS = ["posts"]
+STATIC_PATHS = ["favicon.ico"]
 EXTRA_PATH_METADATA = dict()
 
 # Static files to copy from content to output
-for name in 'CNAME', '404.md', '.nojekyll':
+for name in "CNAME", "404.md", ".nojekyll":
     STATIC_PATHS.append(name)
-    EXTRA_PATH_METADATA[name] = {'path': name}
+    EXTRA_PATH_METADATA[name] = {"path": name}
 
-TIMEZONE = 'America/Los_Angeles'
+TIMEZONE = "America/New_York"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en-us"
 
 # URL settings
-SITEURL = 'https://blog.dhimmel.com'
+SITEURL = "https://blog.dhimmel.com"
 # Only enable document-relative URLs when developing
 RELATIVE_URLS = False
-ARTICLE_URL = '{slug}/'
-ARTICLE_SAVE_AS = '{slug}/index.html'
+ARTICLE_URL = "{slug}/"
+ARTICLE_SAVE_AS = "{slug}/index.html"
 
 USE_FOLDER_AS_CATEGORY = False
 
@@ -32,8 +32,8 @@ SUMMARY_MAX_LENGTH = 75
 
 # Feed generation is usually not desired when developing
 FEED_DOMAIN = SITEURL
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-FEED_ALL_RSS = 'feeds/all.rss.xml'
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+FEED_ALL_RSS = "feeds/all.rss.xml"
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
@@ -41,38 +41,38 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = [
-    ('<b>Satoshi Village Blog</b>', '/'),
-    ('&ndash; archives', '/archives'),
-    ('<b>Daniel Himmelstein</b>', 'https://dhimmel.com/'),
-    ('&ndash; about', 'https://dhimmel.com/about'),
-    ('&ndash; research', 'https://dhimmel.com/research'),
+    ("<b>Satoshi Village Blog</b>", "/"),
+    ("&ndash; archives", "/archives"),
+    ("<b>Daniel Himmelstein</b>", "https://dhimmel.com/"),
+    ("&ndash; about", "https://dhimmel.com/about"),
+    ("&ndash; research", "https://dhimmel.com/research"),
 ]
 
 DEFAULT_PAGINATION = 10
 
 # Pelicanyan
-THEME = 'themes/pelicanyan/'
-TWITTER_ACCOUNT = 'dhimmel'
-DIRECT_TEMPLATES = ('index', 'archives', 'sitemap', 'robots', 'humans')
-ROBOTS_SAVE_AS = 'robots.txt'
-HUMANS_SAVE_AS = 'humans.txt'
-SITEMAP_SAVE_AS = 'sitemap.xml'
-DATE_FORMATS = {'en': '%B %d, %Y'}
+THEME = "themes/pelicanyan/"
+TWITTER_ACCOUNT = "dhimmel"
+DIRECT_TEMPLATES = ("index", "archives", "sitemap", "robots", "humans")
+ROBOTS_SAVE_AS = "robots.txt"
+HUMANS_SAVE_AS = "humans.txt"
+SITEMAP_SAVE_AS = "sitemap.xml"
+DATE_FORMATS = {"en": "%B %d, %Y"}
 SITEDESCRIPTION = "the blog of Daniel Himmelstein"
 TYPOGRIFY = True
 MARKDOWN = {
-    'extension_configs': {
-        'markdown.extensions.toc': {},
-        'markdown.extensions.tables': {},
+    "extension_configs": {
+        "markdown.extensions.toc": {},
+        "markdown.extensions.tables": {},
     },
-    'output_format': 'html5',
+    "output_format": "html5",
 }
 
 # External services
-DISQUS_SITENAME = 'satoshivillage'
+DISQUS_SITENAME = "satoshivillage"
 
 # GOOGLE_ANALYTICS = 'UA-52757861-4'
 # GA_ACCOUNT = 'UA-52757861-4' # used for pelicanyan
 
-PIWIK_URL = 'piwik.dhimmel.com'
+PIWIK_URL = "piwik.dhimmel.com"
 PIWIK_SITE_ID = 5
