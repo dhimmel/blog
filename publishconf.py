88f78ebdaa763f41be1ad50c47bf212453a9ab89
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2025-07-27 16:42:31 -0400

    fix imports

--- publishconf.py
+++ publishconf.py
@@ -1,5 +1,13 @@
-from pelicanconf import *  # noqa: F403
-from pelicanconf import SITEURL
+import sys
+from pathlib import Path
+
+# Add current directory to Python path
+current_dir = Path(__file__).parent
+if str(current_dir) not in sys.path:
+    sys.path.insert(0, str(current_dir))
+
+from pelicanconf import *  # noqa: F403, E402
+from pelicanconf import SITEURL  # noqa: E402
 
 RELATIVE_URLS = False
 DELETE_OUTPUT_DIRECTORY = True
