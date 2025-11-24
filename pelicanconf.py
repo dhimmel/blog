86512ea7a5f0408b894ca1d05b7456e82c094ac2
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2025-11-24 11:38:35 -0500

    remove navbar about page link

--- pelicanconf.py
+++ pelicanconf.py
@@ -45,8 +45,6 @@ LINKS = [
     ("<b>Satoshi Village Blog</b>", "/"),
     ("&ndash; archives", "/archives"),
     ("<b>Daniel Himmelstein</b>", "https://dhimmel.com/"),
-    ("&ndash; about", "https://dhimmel.com/about"),
-    # ("&ndash; research", "https://dhimmel.com/research"),
 ]
 
 DEFAULT_PAGINATION = 10
